a275ac629a51a48f1f976f6c37b8c03d962304ff
iamjarvi <anthony.m.jarvis@gmail.com>
2021-09-14 00:12:37 -0500

    Removed default html and public files, getting WebsiteHandler ready for routing with OpsFlow app.

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -35,9 +35,6 @@ impl Handler for WebsiteHandler {
             // Routing / Endpoints to implement
             Method::GET => match request.path() {
                 "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
-                "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
-                "/test" => Response::new(StatusCode::Ok, self.read_file("test.html")),
-                "/robots.txt" => Response::new(StatusCode::Ok, self.read_file("robots.txt")),
                 path => match self.read_file(path) {
                     Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
                     None => Response::new(StatusCode::NotFound, None),
